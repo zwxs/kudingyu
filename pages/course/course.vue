baaa46bde560f3d30631fa83a8804672704761be
zwxs <601304724@qq.com>
2019-04-15 17:34:18 +0800

    完成首页布局

--- pages/course/course.vue
+++ pages/course/course.vue
@@ -1,19 +1,47 @@
 <template>
-	<view>
-		<text>课程</text>
-	</view>
+	<view><swiper-banner height="100" :data="resData" @click="swiperClick"></swiper-banner></view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
+import swiperBanner from '@/components/wlp-swiper-banner/wlp-swiper-banner.vue';
+
+export default {
+	components: {
+		swiperBanner
+	},
+	data() {
+		return {
+			data: [
+				{
+					imgUrl: '/static/img/banner1.jpg',
+					link: '加载中...'
+				},
+				{
+					imgUrl: '/static/img/banner2.jpg',
+					link: 'www.baidu.com'
+				},
+				{
+					imgUrl: '/static/img/banner3.jpg',
+					link: 'www.taobao.com'
+				}
+			]
+		};
+	},
+	computed: {
+		resData: function() {
+			let obj = [];
+			for (let item of this.data) {
+				obj.push(item.imgUrl);
+			}
+			return obj;
+		}
+	},
+	methods: {
+		swiperClick: function(index) {
+			console.log(this.data[index].link);
 		}
 	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,19 +1,160 @@
 <template>
 	<view>
-		<text>首页111</text>
-		<view></view>
+		<view><mSearch :show="false" @search="search($event, 1)" placeholder="请输入课程名称"></mSearch></view>
+		<view><swiper-banner :height="50" :data="resData" @click="swiperClick"></swiper-banner></view>
+		<view class="recommend">
+			<view class="title">
+				<text class="left">推荐课程</text>
+				<text class="right icon-liebiaojiantou"></text>
+			</view>
+			<view class="content">
+				<scroll-view class="scroll-view-x" scroll-x="true" scroll-left="0">
+					<view class="scroll-view-item"><image src="/static/img/recommend1.jpg" class="scroll-view-item-img"></image></view>
+					<view class="scroll-view-item"><image src="/static/img/recommend1.jpg" class="scroll-view-item-img"></image></view>
+					<view class="scroll-view-item"><image src="/static/img/recommend1.jpg" class="scroll-view-item-img"></image></view>
+				</scroll-view>
+			</view>
+		</view>
+		<view class="hot">
+			<view class="title">
+				<text class="left">推荐视频</text>
+				<text class="right icon-liebiaojiantou"></text>
+			</view>
+			<view class="content">
+				<glanceProductList
+					:data="[
+						{
+							src: '/static/img/Snipaste_2019-02-21_20-55-01.png',
+							imghref: '',
+							name: '可爱的玩偶制作过程',
+							oriprice: '120'
+						},
+						{
+							src: '/static/img/Snipaste_2019-02-21_20-55-01.png',
+							imghref: '',
+							name: '可爱的玩偶制作过程',
+							oriprice: '120'
+						},
+						{
+							src: '/static/img/Snipaste_2019-02-21_20-55-01.png',
+							imghref: '',
+							name: '可爱的玩偶制作过程',
+							oriprice: '120'
+						},
+						{
+							src: '/static/img/Snipaste_2019-02-21_20-55-01.png',
+							imghref: '',
+							name: '可爱的玩偶制作过程',
+							oriprice: '120'
+						}
+					]"
+				></glanceProductList>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+import mSearch from '@/components/mehaotian-search/mehaotian-search.vue';
+import swiperBanner from '@/components/wlp-swiper-banner/wlp-swiper-banner.vue';
+import glanceProductList from '@/components/wxcomponents/glance-ProductList/glance-ProductList.vue';
 export default {
+	components: {
+		mSearch,
+		swiperBanner,
+		glanceProductList
+	},
 	data() {
+		return {
+			val0: '',
+			data: [
+				{
+					imgUrl: '/static/img/banner1.jpg',
+					link: '加载中...'
+				},
+				{
+					imgUrl: '/static/img/banner2.jpg',
+					link: 'www.baidu.com'
+				},
+				{
+					imgUrl: '/static/img/banner3.jpg',
+					link: 'www.taobao.com'
+				}
+			],
+			scrollTop: 0,
+			old: {
+				scrollTop: 0
+			},
+			recommendImg: [
+				{
+					url: '/static/img/recommend1.jpg'
+				},
+				{
+					url: '/static/img/recommend1.jpg'
+				},
+				{
+					url: '/static/img/recommend1.jpg'
+				},
+				{
+					url: '/static/img/recommend1.jpg'
+				}
+			]
+		};
+	},
+
+	onLoad() {
 		return {};
 	},
-	onLoad() {},
-	methods: {}
+	computed: {
+		resData() {
+			let obj = [];
+			for (let item of this.data) {
+				obj.push(item.imgUrl);
+			}
+			return obj;
+		}
+	},
+	methods: {
+		search(e, val) {
+			console.log(e, val);
+			this['val' + val] = e;
+		},
+		swiperClick: function(index) {
+			console.log(this.data[index].link);
+		}
+	}
 };
 </script>
-
-<style></style>
-<style lang="less" scoped></style>
+<style lang="less">
+.recommend,
+.hot {
+	.title {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		line-height: 40px;
+		.left {
+			padding-left: 20px;
+		}
+		.right {
+			padding-right: 20px;
+			font-size: 30px;
+		}
+	}
+}
+.scroll-view-x {
+	width: 100%;
+	white-space: nowrap;
+	padding: 20px 0;
+}
+.scroll-view-item {
+	width: 150px;
+	height: 100px;
+	margin-left: 20px;
+	display: inline-block;
+	.scroll-view-item-img {
+		width: 150px;
+		height: 100%;
+	}
+}
+</style>
